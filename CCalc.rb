66367a1ed07c4bea07d0c6da9371058e2b2db701
Eclipse <0-Eclipse-0@users.noreply.github.com>
2016-02-18 11:44:20 -0500

    Update CCalc.rb

--- CCalc.rb
+++ CCalc.rb
@@ -4,16 +4,16 @@ puts "|-------------------------|"
 puts "|--By: Matthew Hambrecht--|"
 puts "|-------------------------|"
 
-print "\n\n Enter the diameter of the circle: "
+print "\n Enter the diameter of the circle: "
 d = gets.chomp.to_f
 r = d / 2
 r2 = r**2
 pi = 3.14
 c = d * pi
 a = r2 * pi
-print "Diameter = ", d
-print "Radius = ", r
-print "Circumference = ", c
-print "Radius^2 = ", r2
-print "Pi = ", pi
-print "Area = ", a
+print "Diameter = ", d, "\n"
+print "Radius = ", r, "\n"
+print "Circumference = ", c, "\n"
+print "Radius^2 = ", r2, "\n"
+print "Pi = ", pi, "\n"
+print "Area = ", a, "\n"
